279f8f4db066bc1662e555f155ff2a9bcfa48c2b
qq0313 <qiqi0313@outlook.com>
2018-03-06 21:50:21 +0800

    修改注释

--- Sort/src/BubbleSort.java
+++ Sort/src/BubbleSort.java
@@ -9,11 +9,11 @@ import java.util.Arrays;
  **/
 public class BubbleSort {
     public static void main(String[] args) {
-        String path="D:\\Java\\sources.list";
+        String path = "D:\\Java\\sources.list";
 
-     //   String s = readFileByInputStream("D:\\Java\\sources.list");
+        //   String s = readFileByInputStream("D:\\Java\\sources.list");
 
-        String s=FileIOUtils.readByBufferedReader(path);
+        String s = FileIOUtils.readByBufferedReader(path);
 
         String[] strings = s.split(",");
         int[] arr = Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
@@ -42,84 +42,46 @@ public class BubbleSort {
         }
     }
 
-        /**
-         * @描述：字节流的数据读取方式：每次读取一定长度的字节，建议使用
-         * @创建时间：
-         */
-        public static String readFileByInputStream (String fileName){
+    /**
+     * @描述：字节流的数据读取方式：每次读取一定长度的字节，建议使用
+     * @创建时间：
+     */
+    public static String readFileByInputStream(String fileName) {
 
-            String arr = null;
-            FileInputStream fis = null;
+        String arr = null;
+        FileInputStream fis = null;
 
-            try {
+        try {
 
-                fis = new FileInputStream(fileName);
-                // byte[] bs=new byte[1024*4];
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                int len = 0;
-                while ((len = fis.read()) != -1) {
-                    baos.write(len);
-
-                }
-
-                arr = baos.toString();
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                System.err.println("系统找不到指定的文件");
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (fis != null) {
-                    try {
-                        fis.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+            fis = new FileInputStream(fileName);
+            // byte[] bs=new byte[1024*4];
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int len = 0;
+            while ((len = fis.read()) != -1) {
+                baos.write(len);
 
             }
-            return arr;
 
-        }
+            arr = baos.toString();
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.err.println("系统找不到指定的文件");
 
-        public static void inputStreamReader1 () {
-            //创建字节对象
-            InputStream inputStream = null;
-            try {
-                //实例化对象
-                inputStream = new FileInputStream("JavaIOOutputStreamReader.java");
-
-                /**
-                 * 读取一定长度的字节数据，建议使用，可以读取中文（文件中的数据为：OutputStreamp测试写入数据）
-                 * 读取结果为：OutputStreamp测试写入数据
-                 */
-                //定义每次读取字节的大小与保存字节的数据
-                byte[] bs = new byte[1024];
-                //定义每次读取的长度
-                int len = -1;
-                //循环读取数据，如果读取的数据为-1，说明已经读取了末尾
-                while ((len = inputStream.read(bs)) != -1) {
-                    //输出字符
-                    System.out.println(new String(bs, 0, len));
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                //关闭流
-                if (inputStream != null) {
-                    try {
-                        inputStream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
-        }
 
+        }
+        return arr;
 
     }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,32 @@
 import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        String path="D:\\Java\\sources.list";
+        String path = "D:\\Java\\sources.list";
 
-      String content=readFileByBufferedReader(path);
+        String content = readByBufferedReader(path);
         //替换字符串里Fix为haha
-       String s= content.replace("Fix","haha");
-       boolean successful= writeByBufferedWriter(s,path);
+
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String curTime = sdf.format(date);
+        String s = content.replaceAll("^[123]$", curTime);
+        boolean successful = writeByBufferedWriter(s, path);
         System.out.println(successful);
     }
-//    文件读取:通过BufferedReader读取数据
+
+    //    文件读取:通过BufferedReader读取数据
     public static String readByBufferedReader(String fileName) {
-        String read=null;
+        String read = null;
         try {
             File file = new File(fileName);
             // 读取文件，并且以utf-8的形式写出去
-            BufferedReader bufread;
-
-            bufread = new BufferedReader(new FileReader(file));
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufread = new BufferedReader(fileReader);
             while ((read = bufread.readLine()) != null) {
                 System.out.println(read);
             }
@@ -33,25 +39,24 @@ public class Main {
         return read;
     }
 
-    public static boolean writeByBufferedWriter(String content,String fileName){
+    public static boolean writeByBufferedWriter(String content, String fileName) {
         try {
-            File file= new File(fileName);
+            File file = new File(fileName);
 
-            if (!file.exists()){
+            if (!file.exists()) {
                 file.createNewFile();
             }
             //true时允许追加
             //FileWriter fileWriter=new FileWriter(file,true);
-            FileWriter fileWriter=new FileWriter(file);
+            FileWriter fileWriter = new FileWriter(file);
 
-            BufferedWriter bufferedWriter=new BufferedWriter(fileWriter);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             bufferedWriter.write(content);
             bufferedWriter.flush();
             bufferedWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return true;
     }
 }
